1c19420e719e724ceafc48b74e2353e751cc710f
Elpeor <kotetorrerami@gmail.com>
2025-09-11 17:02:18 -0300

    desarrollo en productos

---
+++ Evaluación1/Scripts/producto.js
@@ -0,0 +1,8 @@
+var productos = [
+    {"id":1, "nombre":"lasagna","tipo":"pasta", "precio":9990, "imagen":"IMG/lasagna.jpg"},
+    {"id":2, "nombre":"margarita","tipo":"pizza", "precio":7990, "imagen":"ING/pizza.jpg"}
+];
+
+document.getElementById("pizza1").setAttribute("src", "IMG/pizza.jpg")
+document.getElementById("pizza1").setAttribute("width", "250")
+document.getElementById("pizza1").setAttribute("heigtht", "100")
