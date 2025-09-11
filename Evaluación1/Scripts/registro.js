efc2fa18543c4fc03c7405aea730909320a28bc8
Elpeor <kotetorrarami@gmail.com>
2025-09-11 09:46:16 -0300

    Añadiendo registro

---
+++ Evaluación1/Scripts/registro.js
@@ -0,0 +1,12 @@
+document.getElementById("select1").addEventListener("change", function(e){
+    var Optsel = e.target.value;
+    console.log(Optsel)
+    if (Optsel != null){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+});
