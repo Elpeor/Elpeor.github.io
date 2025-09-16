52a1fa8f3ca6212ae6417d1d2ab9d08f5a3ef402
lobosvalenzuela <t.lobos@duocuc.cl>
2025-09-16 20:43:56 -0300

    carrrito contacto

--- Evaluación1/Scripts/contacto.js
+++ Evaluación1/Scripts/contacto.js
@@ -54,3 +54,13 @@ document.getElementById("contactoForm").addEventListener("submit", function(e){
         e.target.reset();
     }
 })
+let iconoCarro = document.querySelector(".icon-cart")
+let cerrarCarro = document.querySelector(".cerrar");
+let body = document.querySelector("body");
+// Listener para abrir y cerrar el carro
+iconoCarro.addEventListener("click", ()=>{
+    body.classList.toggle("mostrarCarro")
+})
+cerrarCarro.addEventListener("click", ()=>{
+    body.classList.toggle("mostrarCarro")
+})
